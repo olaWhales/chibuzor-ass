003fa16eb3c0a76296a9671c1c94606739adaf8b
olawhales <ajaditaoreed@gmail.com>
2024-06-23 10:42:28 +0100

    adding assignment

---
+++ DisplayLoopUsingNested.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class DisplayLoopUsingNested{
+	public static void main(String [] args){
+	Scanner input = new Scanner(System.in);
+	
+	//for(int i = 1 ; i <= 10 ; i++){
+	//	System.out.println(i);	
+	//}
+
+	/*int sum = 0 ;
+	int num = 10;
+	for(num = 1 ; num <= 10 ; num++){
+		sum += num ;
+		System.out.println(num);	
+	}
+	System.out.print("The total number is " + sum);
+
+	*/
+
+
+	
+	/*System.out.print("Enter new number ");
+	int number = input.nextInt();
+	System.out.print("Enter another new number ");
+	int number1 = input.nextInt();
+
+	int add = number + number1 ;
+	System.out.print("The total numbers is " + add);
+	*/
+
+
+	int i = 1 ;
+	for(i = 0 ; i <= 5 ; i--){
+		System.out.print(i);
+}
+
+
+
+	}
+}
